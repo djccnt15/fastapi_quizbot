f1ac64e3273994683962942309c6b0d750284196
Jay.D <djccnt15@gmail.com>
2025-04-25 23:49:07 +0900

    create DomainEnum in enums, add bot router to api router

---
+++ src/domain/enums.py
@@ -0,0 +1,5 @@
+from enum import StrEnum, auto
+
+
+class DomainEnum(StrEnum):
+    BOT = auto()
--- src/domain/router.py
+++ src/domain/router.py
@@ -1,7 +1,13 @@
 from fastapi import APIRouter
 
+from .bot import bot_controller
 from .default import router as default_router
+from .enums import DomainEnum
 
 router = APIRouter(prefix="/api")
 
 router.include_router(router=default_router)
+router.include_router(
+    router=bot_controller.router,
+    tags=[DomainEnum.BOT],
+)
