9b0d7ae72aedf52ae3b55eb4802dd73c4fb5e272
Jay.D <djccnt15@gmail.com>
2025-04-26 14:37:34 +0900

    update main create on_startup event for auto-create DB tables

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
 
+from src.db import entity
 from src.domain.router import router
 
 app = FastAPI()
 app.include_router(router=router)
+
+
+@app.on_event("startup")
+def on_startup():
+    from src.configuration.database import engine
+
+    entity.Base.metadata.create_all(bind=engine)
