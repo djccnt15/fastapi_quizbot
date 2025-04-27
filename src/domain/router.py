53152f7d28ecd2c28ff124dd91df0f65cbe0eea3
Jay.D <djccnt15@gmail.com>
2025-04-27 15:50:04 +0900

    update routing structure of default apis

--- src/domain/router.py
+++ src/domain/router.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter
 
 from .bot import bot_controller
-from .default import router as default_router
+from .default import default_controller
 from .enums import DomainEnum
 
-router = APIRouter(prefix="/api")
+API_PREFIX = "/api"
 
-router.include_router(router=default_router)
+router = APIRouter()
+
+router.include_router(router=default_controller.router)
 router.include_router(
+    prefix=API_PREFIX,
     router=bot_controller.router,
     tags=[DomainEnum.BOT],
 )
