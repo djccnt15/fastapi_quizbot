06db70ca2099e60ec71c5ef8d4679e86e9595b0e
Jay.D <djccnt15@gmail.com>
2025-04-30 22:29:11 +0900

    create BaseMixin for entity, QuizEntity

--- src/db/entity.py
+++ src/db/entity.py
@@ -1,9 +1,29 @@
-from sqlalchemy import BigInteger, Column, Integer, String
+from sqlalchemy import (
+    BigInteger,
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    Text,
+    func,
+)
+from sqlalchemy.orm import relationship
 
 from src.configuration.database import Base
 
 
-class UserEntity(Base):
+class BaseMixin:
+    created_at = Column(DateTime, nullable=False, default=func.utc_timestamp())
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=func.utc_timestamp(),
+        onupdate=func.utc_timestamp(),
+    )
+
+
+class UserEntity(BaseMixin, Base):
     __tablename__ = "user"
 
     id = Column(BigInteger, primary_key=True, autoincrement=False)
@@ -11,3 +31,17 @@ class UserEntity(Base):
     first_name = Column(String(100))
     last_name = Column(String(100))
     score = Column(Integer, default=0)
+    quiz_id = Column(Integer, ForeignKey("quiz.id"), nullable=True)
+
+    quiz = relationship("QuizEntity", back_populates="current_users", uselist=False)
+
+
+class QuizEntity(BaseMixin, Base):
+    __tablename__ = "quiz"
+
+    id = Column(Integer, primary_key=True)
+    question = Column(Text, nullable=False)
+    content = Column(Text, nullable=False)
+    answer = Column(Integer, nullable=False)
+
+    current_users = relationship("UserEntity", back_populates="quiz")
