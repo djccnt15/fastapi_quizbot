b53d51a27eb463b6d82d759337b0c385c9b2e2fe
Jay.D <djccnt15@gmail.com>
2025-05-01 16:48:10 +0900

    update entity module update field to Mapped type

--- src/db/entity.py
+++ src/db/entity.py
@@ -1,23 +1,18 @@
-from sqlalchemy import (
-    BigInteger,
-    Column,
-    DateTime,
-    ForeignKey,
-    Integer,
-    String,
-    Text,
-    func,
-)
-from sqlalchemy.orm import relationship
+from datetime import datetime
+
+from sqlalchemy import BigInteger, DateTime, ForeignKey, Integer, String, Text, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.configuration.database import Base
 
 
 class BaseMixin:
-    created_at = Column(DateTime, nullable=False, default=func.utc_timestamp())
-    updated_at = Column(
+    created_at: Mapped[datetime] = mapped_column(
         DateTime,
-        nullable=False,
+        default=func.utc_timestamp(),
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
         default=func.utc_timestamp(),
         onupdate=func.utc_timestamp(),
     )
@@ -26,12 +21,12 @@ class BaseMixin:
 class UserEntity(BaseMixin, Base):
     __tablename__ = "user"
 
-    id = Column(BigInteger, primary_key=True, autoincrement=False)
-    username = Column(String(100))
-    first_name = Column(String(100))
-    last_name = Column(String(100))
-    score = Column(Integer, default=0)
-    quiz_id = Column(Integer, ForeignKey("quiz.id"), nullable=True)
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=False)
+    username: Mapped[str | None] = mapped_column(String(100))
+    first_name: Mapped[str | None] = mapped_column(String(100))
+    last_name: Mapped[str | None] = mapped_column(String(100))
+    score: Mapped[int] = mapped_column(Integer, default=0)
+    quiz_id: Mapped[int | None] = mapped_column(Integer, ForeignKey("quiz.id"))
 
     quiz = relationship("QuizEntity", back_populates="current_users", uselist=False)
 
@@ -39,9 +34,9 @@ class UserEntity(BaseMixin, Base):
 class QuizEntity(BaseMixin, Base):
     __tablename__ = "quiz"
 
-    id = Column(Integer, primary_key=True)
-    question = Column(Text, nullable=False)
-    content = Column(Text, nullable=False)
-    answer = Column(Integer, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    question: Mapped[str] = mapped_column(Text)
+    content: Mapped[str] = mapped_column(Text)
+    answer: Mapped[str] = mapped_column(Integer)
 
     current_users = relationship("UserEntity", back_populates="quiz")
