f3f231f01872a87acba3321bff6abe66482bda78
Jay.D <djccnt15@gmail.com>
2025-05-02 20:36:31 +0900

    add CORSMiddleware

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.db import entity
 from src.domain.router import router
 
 app = FastAPI()
 app.include_router(router=router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["OPTIONS", "GET", "POST"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
