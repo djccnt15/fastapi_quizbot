390ddf1631b3336da427b3affd35f8f9dc293ca0
Jay.D <djccnt15@gmail.com>
2025-05-03 21:11:17 +0900

    create index redirect to api doc

--- src/domain/default/default_controller.py
+++ src/domain/default/default_controller.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from starlette.responses import RedirectResponse
 
 router = APIRouter()
 
@@ -6,3 +7,8 @@ router = APIRouter()
 @router.get("/health")
 async def health():
     return 1
+
+
+@router.get(path="/", response_class=RedirectResponse)
+async def index_redirect():  # temporal index page redirect to swagger
+    return "/docs"
