78f23946fe101f5358017f7fd2ec5a4fa2867fc3
Jay.D <djccnt15@gmail.com>
2025-05-06 16:15:51 +0900

    update test_main add test for health api

--- test/test_main.py
+++ test/test_main.py
@@ -3,6 +3,12 @@ from httpx import AsyncClient
 
 
 @pytest.mark.asyncio
-async def test_index(default_client: AsyncClient):
+async def test_health(default_client: AsyncClient):
     r = await default_client.get("/health")
     assert r.status_code == 200
+
+
+@pytest.mark.asyncio
+async def test_index(default_client: AsyncClient):
+    r = await default_client.get("/")
+    assert r.status_code == 307
